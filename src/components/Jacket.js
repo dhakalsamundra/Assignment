be293eea7180b468b9c4e9e71b07b87c52a9d024
dhakalsamundra <seapush02@gmail.com>
2020-11-29 19:55:01 +0200

    use map and filter to assign

--- src/components/Jacket.js
+++ src/components/Jacket.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Card, CardContent, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import EuroIcon from '@material-ui/icons/Euro'
-
 import useProducts from '../hooks/useProducts'
 import Spinner from './Spinner'
+import useAvailabilities from '../hooks/useAvailabilities'
 
 const useStyles = makeStyles({
   blogCard: {
@@ -15,17 +15,51 @@ const Jacket = () => {
   const [products, loading] = useProducts(
     'https://bad-api-assignment.reaktor.com/products/jackets'
   )
+  const [ availabilities, isLoading ] = useAvailabilities()
   const classes = useStyles()
+
+  const sortedProducts = products.sort((a, b) => (a.manufacturer > b.manufacturer) ? 1 : -1)
+  console.log('afadfdafea', availabilities)
+
+  let resultData = []
+
+  if(availabilities.length > 2) {
+    // eslint-disable-next-line array-callback-return
+    resultData = sortedProducts.map(product => {
+      if(product.manufacturer === 'abiplos'){
+        const matchedItems = availabilities && availabilities[0].filter(item => item.id.toLowerCase() === product.id)
+        return Object.assign({}, product, { status: getStatus(matchedItems) })
+      } else if(product.manufacturer === 'derp'){
+        const matchedItems = availabilities[1].filter(item => item.id.toLowerCase() === product.id)
+        return Object.assign({}, product, { status: getStatus(matchedItems) })
+      } else if(product.manufacturer === 'nouke'){
+        const matchedItems = availabilities[2].filter(item => item.id.toLowerCase() === product.id)
+        return Object.assign({}, product, { status: getStatus(matchedItems) })
+      } else if(product.manufacturer === 'reps'){
+        const matchedItems = availabilities[3].filter(item => item.id.toLowerCase() === product.id)
+        return Object.assign({}, product, { status: getStatus(matchedItems) })
+      } else if(product.manufacturer === 'xoon') {
+        const matchedItems = availabilities[4].filter(item => item.id.toLowerCase() === product.id)
+        return Object.assign({}, product, { status: getStatus(matchedItems) })
+      }
+    })
+    // eslint-disable-next-line no-inner-declarations
+    function getStatus(matchedItems){
+      const newString = matchedItems[0].DATAPAYLOAD.slice(31)
+      const ourArray = newString.split('')
+      const statusInfo = ourArray.slice(0,ourArray.findIndex(((element) => element === '<')))
+      return statusInfo.join('')
+    }
+  }
 
 
 
   return (
     <div>
-      {( loading) ? (
+      {(isLoading || loading) ? (
         <Spinner />
-
       ) : (
-        products.map((jacket) => (
+        resultData.map((jacket) => (
           <Card className={classes.blogCard} key={jacket.id}>
             <CardContent>
               <Typography gutterBottom variant='h6'>

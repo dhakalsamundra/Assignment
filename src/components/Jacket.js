a002dbc39e277b66e70cbef2a806efde3c163659
dhakalsamundra <seapush02@gmail.com>
2020-11-29 19:32:41 +0200

    add jacket component

---
+++ src/components/Jacket.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import EuroIcon from '@material-ui/icons/Euro'
+
+import useProducts from '../hooks/useProducts'
+import Spinner from './Spinner'
+
+const useStyles = makeStyles({
+  blogCard: {
+    marginTop: 20,
+  },
+})
+const Jacket = () => {
+  const [products, loading] = useProducts(
+    'https://bad-api-assignment.reaktor.com/products/jackets'
+  )
+  const classes = useStyles()
+
+
+
+  return (
+    <div>
+      {( loading) ? (
+        <Spinner />
+
+      ) : (
+        products.map((jacket) => (
+          <Card className={classes.blogCard} key={jacket.id}>
+            <CardContent>
+              <Typography gutterBottom variant='h6'>
+                  Name: {jacket.name}
+              </Typography>
+              <Typography gutterBottom variant='h6'>
+                <EuroIcon />: {jacket.price}
+              </Typography>
+              <Typography gutterBottom variant='h6'>
+                  Brand: {jacket.manufacturer}
+              </Typography>
+              <Typography gutterBottom variant='h6'>
+                  TYPES: {jacket.type}
+              </Typography>
+              <Typography gutterBottom variant='h6'>
+                  Status: {jacket.status}
+              </Typography>
+              <Typography gutterBottom variant='h6'>
+                  Color:{' '}
+                {jacket.color.map((element) => (
+                  <li key={element}>{element}</li>
+                ))}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))
+      )}
+    </div>
+  )
+}
+export default Jacket
